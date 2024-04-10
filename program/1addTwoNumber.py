aaf3cf8fb2076cbf434fde4d3c9c292361e12611
yash00369 <yash.askoo9@gmail.com>
2024-04-10 19:30:18 +0530

    python basic program

---
+++ program/1addTwoNumber.py
@@ -0,0 +1,16 @@
+# solution with predefined variable
+
+# num1=30
+# num2=40
+
+# num3=num1+num2
+# print ("value of sum =" , num3)
+
+
+# solution through user input
+
+num1=float(input('Enter the number:'))
+num2=float(input('Enter the number:'))
+sum=num1+num2
+print('The Sum is =', sum)
+
---
+++ program/2squareRoot.py
@@ -0,0 +1,12 @@
+# Solution using exponential 
+
+num1 =int(input('Enter the number here:'))
+sqr=num1**(1/2)
+print('the sqaure root of given number:',sqr)
+
+
+# solution using math module
+import math
+num = int(input('Enter a number here:'))
+sqr=math.sqrt(num)
+print('the sqaure root of given number is:',sqr)
